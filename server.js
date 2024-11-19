fc1b5a4f593d7c25f49c2ff8fec1b9c64ead0b2f
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2024-11-19 15:30:04 -0300

    Aula 02: Criando e Estruturando sua Primeira API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Gato fofo dormindo",
+        imagem: "https://placekitten.com/400/200"
+    },
+    {
+        id: 3,
+        descricao: "Paisagem com um gato",
+        imagem: "https://picsum.photos/seed/cats/600"
+    },
+    {
+        id: 4,
+        descricao: "Gato brincando com um novelo de lã",
+        imagem: "https://source.unsplash.com/random/600x400/?cat,toy"
+    },
+    {
+        id: 5,
+        descricao: "Gato olhando pela janela",
+        imagem: "https://unsplash.com/photos/2d4lAQAlbqs"
+    },
+    {
+        id: 6,
+        descricao: "Gato preto em uma noite escura",
+        imagem: "https://www.pexels.com/photo/black-cat-on-a-black-background-1015554/"
+    }
+];
+
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api",(req, res) => {
-    res.status(200).send("Boas vindas à imersão!");
+app.get("/posts",(req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorID(id){
+    return posts.findIndex((post) =>{
+        return post.id === Number(id)
+    });
+}
+
+app.get("/posts/:id",(req, res) => {
+    const index = buscarPostPorID(req.params.id);
+    res.status(200).json(posts[index]);
 });
