06620f05ee92f9a9b020538c1ff32f3fa5fcf6cc
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2024-11-21 16:41:42 -0300

    Aula 04: Implementando Armazenamento e Upload de Imagens

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -4,11 +4,17 @@ import conectarAoBanco from "../config/dbConfig.js";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para buscar todos os posts do banco de dados
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     // Obtém o banco de dados 'imersao-instabytes'
     const db = conexao.db("imersao-instabytes");
     // Obtém a coleção 'posts'
     const colecao = db.collection("posts");
     // Retorna todos os documentos da coleção como um array
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,14 +1,39 @@
-import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import express from "express"; // Importa o framework Express para criar a API
+import multer from "multer"; // Importa o middleware Multer para lidar com uploads de arquivos
 
-const routes = (app) =>{
-    // Habilita o parsing de JSON no corpo das requisições
-    app.use(express.json());
-    // Rota GET para buscar todos os posts
-    app.get("/posts", listarPosts);
-}
+// Importa funções controladoras de postsController.js (provavelmente contendo lógica de banco de dados)
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+// Configura armazenamento em disco para arquivos enviados
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Define o diretório de destino para arquivos (`./uploads` ou `uploads/`)
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    // Define o nome do arquivo salvo (mantém o nome original)
+    cb(null, file.originalname);
+  },
+});
+
+// Cria uma instância do Multer usando a configuração de armazenamento e destino
+const upload = multer({ dest: "./uploads", storage });
+
+// Define rotas para a API
+const routes = (app) => {
+  // Habilita o parsing de dados JSON no corpo das requisições
+  app.use(express.json());
+
+  // Rota GET para listar todos os posts
+  app.get("/posts", listarPosts);
+
+  // Rota POST para criar um novo post
+  app.post("/posts", postarNovoPost);
+
+  // Rota POST para upload de imagem
+  // Usa o middleware `upload.single("imagem")` para processar uploads de um único arquivo chamado "imagem"
+  // Após upload bem-sucedido, chama a função `uploadImagem` para lidar com a imagem
+  app.post("/upload", upload.single("imagem"), uploadImagem);
+};
 
 export default routes;
-
-
-
